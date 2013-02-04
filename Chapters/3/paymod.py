135c05e5c2e38a77a34e743cc87702cd26aff8b8
b3m <bminear3@gmail.com>
2013-02-04 09:19:08 -0500

    Restructuring classes.

--- Chapters/3/paymod.py
+++ Chapters/3/paymod.py
@@ -9,6 +9,10 @@ class Rates(object):
 class Item(object):
 	"""Item information"""
 	
+
+class Account(object):
+	"""Account information"""
+
 	def __init__(self, price, downRate, intRate, payRate):
 		self.basePrice = price
 		self.downPay = price * downRate
@@ -16,6 +20,8 @@ class Item(object):
 		self.startPrice = self.basePrice - self.downPay
 		self.principal = price * payRate
 		self.payment = self.principal + self.interest
+		self.totalBalance = None
+		self.balRemaining = None
 
 	def __str__(self):
 		return 'Base price: ' + str(self.basePrice) + \
@@ -23,17 +29,12 @@ class Item(object):
 		'\nStart Price: ' + str(self.startPrice) + \
 		'\nInterest: ' + str(self.interest) + \
 		'\nPrincipal: ' + str(self.principal) + \
-		'\nPayment: ' + str(self.payment)
+		'\nPayment: ' + str(self.payment) + \
+		'\nTotal Balance: ' + str(self.totalBalance)
 
 	def getPrice(self):
 		return self.startPrice
 
-class Account(object):
-	"""Account information"""
-
-	def __init__(self, price, payment):
+	def setBalance(self, price, payment):
 		self.totalBalance = price - payment
 		self.balRemaining = None
-
-	def __str__(self):
-		return 'Total Balance: ' + str(self.totalBalance)
