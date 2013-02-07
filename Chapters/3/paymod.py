0b4dbaa9131bd8f8f398a00b8ec7d3fea768d463
b3m <bminear3@gmail.com>
2013-02-07 12:11:48 -0500

    Reworking object structure... again!

--- Chapters/3/paymod.py
+++ Chapters/3/paymod.py
@@ -2,76 +2,74 @@ class Rates(object):
 	"""Rate information"""
 
 	def __init__(self):
-		self.downPay = .10
-		self.interest = .01
-		self.payment = .05
+		self.downRate = .10
+		self.intRate = .01
+		self.payRate = .05
 
-	def rateCalc(self, price):
+	def listRates(self):
 		"""[0]Down Payment [1]Interest [2]Monthly Payment"""
-		rates = [
-			self.downPay * price,
-			self.interest * price,
-			self.payment * (price - (price * self.downPay))
-			]
+		rates = [self.downRate, self.intRate, self.payRate]
 		return rates
 
 class Account(object):
 	"""Account information"""
 
 	def __init__(self):
-		self.basePrice = None
-		self.downPay = None
-		self.interest = None
-		self.startPrice = None
-		self.principal = None
-		self.payment = None
+		self.downPayment = None
+		self.base = None
+		self.currentBalance = None
+		self.monthlyPrincipal = None
+		self.monthlyInterest = None
+		self.monthlyPayment = None
 
-	def __str__(self):
-		"""For debugging"""
-		return 'Base price: ' + str(self.basePrice) + \
-		'\nDown Payment: ' + str(self.downPay) + \
-		'\nStart Price: ' + str(self.startPrice) + \
-		'\nInterest: ' + str(self.interest) + \
-		'\nPrincipal: ' + str(self.principal) + \
-		'\nPayment: ' + str(self.payment) + \
-		'\nTotal Balance: ' + str(self.totalBalance)
+	# def __str__(self):
+	# 	"""For debugging"""
+	# 	return 'Base price: ' + str(self.basePrice) + \
+	# 	'\nDown Payment: ' + str(self.downPay) + \
+	# 	'\nStart Price: ' + str(self.startPrice) + \
+	# 	'\nInterest: ' + str(self.interest) + \
+	# 	'\nPrincipal: ' + str(self.principal) + \
+	# 	'\nPayment: ' + str(self.payment) + \
+	# 	'\nTotal Balance: ' + str(self.totalBalance)
 
-	def accCheck(self):
-		if self.basePrice != None:
-			return 0
+	# def accCheck(self):
+	# 	if self.basePrice != None:
+	# 		return 0
 
 	def setPrice(self, price, rates):
 		"""Sets price and related payments"""
-		self.basePrice = price
-		self.downPay = rates[0] 
-		self.interest = rates[1]
-		self.startPrice = self.basePrice - self.downPay
-		self.principal = rates[2]
-		self.payment = self.principal + self.interest
+		self.downPayment = price * rates[0]
+		self.base = price - self.downPayment
+		self.currentBalance = self.base
+		self.monthlyPrincipal = self.base * rates[1]
+		self.monthlyInterest = self.currentBalance * rates[2]
+		self.monthlyPayment = self.monthlyPrincipal + self.monthlyInterest
 
- 	def loanLength(self):
-		payTotal = [self.payment]
-		while payTotal[-1] < self.startPrice:
-			payTotal.append(payTotal[-1] + self.payment)
-		return len(payTotal)
+#  	def loanLength(self):
+# 		payTotal = [self.payment]
+# 		while payTotal[-1] < self.curPrice:
+# 			payTotal.append(payTotal[-1] + self.payment)
+# 		return len(payTotal)
 
-	def monthlyValues(self, month):
-		balRemain = self.startPrice - self.payment * (month + 1)
-		if balRemain == 0 or balRemain < 0:
-			return '%5d%14.2f%16.2f%10.2f%8.2f%18.2f' % \
-			(month + 1,
-			self.startPrice - self.payment * month,
-			self.interest,
-			self.principal,
-			self.startPrice - self.payment * month,
-			0)
-		else:
-			return '%5d%14.2f%16.2f%10.2f%8.2f%18.2f' % \
-			(month + 1,
-			self.startPrice - self.payment * month,
-			self.interest,
-			self.principal,
-			self.payment,
-			balRemain)
+# 	def monthlyValues(self, month):
+# 		self.payment = 
+# 		balRemain = self.startPrice - self.payment * (month + 1)
+# 		self.curPrice = self.startPrice - self.payment * month
+# 		if balRemain == 0 or balRemain < 0:
+# 			return '%5d%14.2f%16.2f%10.2f%8.2f%18.2f' % \
+# 			(month + 1,
+# 			curPrice,
+# 			self.interest * curPrice,
+# 			self.principal,
+# 			curPrice,
+# 			0)
+# 		else:
+# 			return '%5d%14.2f%16.2f%10.2f%8.2f%18.2f' % \
+# 			(month + 1,
+# 			curPrice,
+# 			self.interest * curPrice,
+# 			self.principal,
+# 			self.principal + (self.interest * curPrice),
+# 			balRemain)
 
-# Calculate interest based on current balance.
+# # Calculate interest based on current balance.
--- Chapters/3/test2.py
+++ Chapters/3/test2.py
@@ -1,22 +1,68 @@
 #!/usr/bin/python
+from paymod import Rates, Account
 
-INT_RATE = .01
-DOWN_RATE = .10
-PAY_RATE = .05
+rates = Rates()
+acc = Account()
 
-price = input('Enter a price: ')
-downPayment = price * DOWN_RATE
-base = price - downPayment
-balanceRemaining = base
-monthlyPrincipal = base * PAY_RATE
-monthlyInterest = balanceRemaining * INT_RATE
-monthlyPayment = monthlyPrincipal + monthlyInterest
-i = 1
+def mainMenu():
+	print 'What would you like to do?'
+	print '1). Enter price'
+	print '2). Print table'
+	print '3). Exit'
+	while True:
+		try:
+			menuAns = input('--> ')
+			if menuAns < 1 and menuAns > 3:
+				print 'Invalid input.'
+				tryAgain = raw_input('Press ENTER to continue.')
+			else:
+				return menuAns
+		except (NameError, SyntaxError):
+			print 'Invalid input.'
+			tryAgain = raw_input('Press ENTER to continue.')
 
-while balanceRemaining > 0.00:
-	print i, balanceRemaining, '\t', monthlyPrincipal, '\t', monthlyInterest,'\t', monthlyPayment
-	balanceRemaining = balanceRemaining - monthlyPayment
-	monthlyInterest = balanceRemaining * INT_RATE
-	monthlyPayment = monthlyPrincipal + monthlyInterest
-	i += 1
+def main(menuAns):
+	if menuAns == 1:
+		while True:
+			try:
+				price = input('Enter a price: ')
+				if price < 0:
+					print 'Invalid input'
+					tryAgain = raw_input('Press ENTER to continue.')
+				else:
+					acc.setPrice(price, rates.listRates())
+					print 'Price is:', round(price, 2)
+					moveAlong = raw_input('Press ENTER to continue.')
+					break
+			except (NameError, SyntaxError):
+					print 'Invalid input.'
+					tryAgain = raw_input('Press ENTER to continue.')
 
+	elif menuAns == 2:
+		i = 1
+		print '%5s%15s%17s%11s%9s%19s' % \
+			('Month', 'Current Total', 'Interest Amount',
+			 'Principal', 'Payment', 'Balance Remaining')
+		print '%5s%15s%17s%11s%9s%19s' % \
+			('-' * 5, '-' * 13, '-' * 15, '-' * 9,
+			'-' * 7, '-' * 17)
+
+		while acc.currentBalance > 0.00:
+			balanceRemaining = acc.currentBalance - acc.monthlyPayment
+			if balanceRemaining < 0:
+				balanceRemaining = 0
+				acc.monthlyPayment = acc.currentBalance
+			print '%5s%15.2f%17.2f%11.2f%9.2f%19.2f' % \
+			(i, acc.currentBalance, acc.monthlyInterest, acc.monthlyPrincipal,
+				acc.monthlyPayment, balanceRemaining)
+			acc.currentBalance = acc.currentBalance - acc.monthlyPayment
+			acc.monthlyInterest = balanceRemaining * rates.intRate
+			acc.monthlyPayment = acc.monthlyPrincipal + acc.monthlyInterest
+			i += 1
+		moveAlong = raw_input('Press ENTER to continue.')
+
+	elif menuAns == 3:
+		exit()
+
+while True:
+	main(mainMenu())
