7d74d487515f2142248a001ade5223aa877015cb
Bill <bminear3@gmail.com>
2013-03-11 14:15:38 -0400

    Was using a dictionary to store coordinates until I realized that keys have to be different. Switched to appending a list with 'row:move' and splitting on the : when the values are needed.

--- Chapters/5/test.py
+++ Chapters/5/test.py
@@ -1,10 +1,10 @@
 
 
-table = [['|_|', '|_|', '|_|', '|_|', '|_|', '|_|', '|_|'], 
+table = [['|O|', '|_|', '|_|', '|X|', '|_|', '|_|', '|_|'], 
 		['|_|', '|_|', '|_|', '|_|', '|_|', '|_|', '|_|'],
+		['|_|', '|_|', '|O|', '|_|', '|_|', '|O|', '|_|'],
 		['|_|', '|_|', '|_|', '|_|', '|_|', '|_|', '|_|'],
-		['|_|', '|_|', '|_|', '|_|', '|_|', '|_|', '|_|'],
-		['|_|', '|_|', '|_|', '|X|', '|_|', '|_|', '|_|'],
+		['|_|', '|X|', '|_|', '|X|', '|_|', '|_|', '|_|'],
 		['|_|', '|_|', '|_|', '|X|', '|X|', '|_|', '|X|']]
 
 # x = 0
@@ -31,7 +31,7 @@ table = [['|_|', '|_|', '|_|', '|_|', '|_|', '|_|', '|_|'],
 # 			return False 
 
 # Init all moves dictionary.
-allMoveCoordinates = {}
+allMoveCoordinates = [] 
 # Loop through tables in table.
 for i in xrange((len(table) -1),-1,-1):
 	# Loop through the indecies of each table.
@@ -39,54 +39,67 @@ for i in xrange((len(table) -1),-1,-1):
 		# Search for X or O (moves).
 		if table[i][item] == '|X|' or table[i][item] == '|O|':
 			# If match: append to all moves dictionary.
-			# Key:Value = moveIndex:Row
-			allMoveCoordinates.update({int(item):int(i)})
+			# Key:Value = row:move index
+			allMoveCoordinates.append(str(i)+':'+str(item))
 			print allMoveCoordinates
 
-for move, row in allMoveCoordinates.iteritems():
-	# print move, row
-	# print table[row], table[row][move]
+# Iterating through the coordinates of moves stored in allMoveCoordinates.
+# Looking for matching move value three indices away.
+for eachSet in allMoveCoordinates:
+	# Splits list item into [0] for row, [1] for move index.
+	moveCoordinates = eachSet.split(':')
+	moveCoordinates = map(int, moveCoordinates)
 	try:
-		value = move - 9 
+		# Stores X or O based on coords.
+		playerMarker = table[moveCoordinates[0]][moveCoordinates[1]]
+		value = moveCoordinates[1] - 3 
 		print value
-		if table[row][(value)] >= 0 and table[row][(value)] == ('|X|' or '|O|'):
+		# Checks value for positive index reference and matching move.
+		if value >= 0 and table[moveCoordinates[0]][value] == (playerMarker):
 			print 'win!'
+		# If index reference is less than zero do nothing.
 		elif value < 0:
 			print 'less than zero'
 	except IndexError:
 		print 'Invalid index.'
 
+# """
+# Function: tableParse()
+# 	+) Parses table and returns move coordinates in dictionary.
+# 		+) Move coordinates are in format:
+# 			+) Key:Value = Move:
+# """
 
-"""
-Parse entire table:
-	Store moves (X and O) in dictionary (move:row)
-		For each pair check:
-			move - 3 == left
-			move + 3 == right
-			row - 3  == up
-			row + 3  == down
-			(row - 3) and (move - 3)
-				== up, left
-			(row - 3) and (move + 3)
-				== up, right
-			(row + 3) and (move - 3)
-				== down, left
-			(row + 3) and (move + 3)
-				== down, right
-			If the values (X/O) match:
-				Push in-between values into list:
-					move - (1/2) == left
-					move + (1/2) == right
-					row - (1/2)  == up
-					row + (1/2)  == down
-					(row - (1/2)) and (move - (1/2))
-						== up, left
-					(row - (1/2)) and (move + (1/2))
-						== up, right
-					(row + (1/2)) and (move - (1/2))
-						== down, left
-					(row + (1/2)) and (move + (1/2))
-						== down, right
+# """
+# Parse entire table:
+# 	Store moves (X and O) in dictionary (move:row)
+# 		For each pair check:
+# 			move - 3 == left
+# 			move + 3 == right
+# 			row - 3  == up
+# 			row + 3  == down
+# 			(row - 3) and (move - 3)
+# 				== up, left
+# 			(row - 3) and (move + 3)
+# 				== up, right
+# 			(row + 3) and (move - 3)
+# 				== down, left
+# 			(row + 3) and (move + 3)
+# 				== down, right
+# 			If the values (X/O) match:
+# 				Push in-between values into list:
+# 					move - (1/2) == left
+# 					move + (1/2) == right
+# 					row - (1/2)  == up
+# 					row + (1/2)  == down
+# 					(row - (1/2)) and (move - (1/2))
+# 						== up, left
+# 					(row - (1/2)) and (move + (1/2))
+# 						== up, right
+# 					(row + (1/2)) and (move - (1/2))
+# 						== down, left
+# 					(row + (1/2)) and (move + (1/2))
+# 						== down, right
 
 
-"""
+# """
