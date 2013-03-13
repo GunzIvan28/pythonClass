38d4ea6dd446b03fde1b70b086f7483c0d6a782a
Bill <bminear3@gmail.com>
2013-03-13 02:25:11 -0400

    Added in some input sanitization.

--- Chapters/5/connectFour.py
+++ Chapters/5/connectFour.py
@@ -11,25 +11,37 @@ table = [['|_|', '|_|', '|_|', '|_|', '|_|', '|_|', '|_|'],
 		['|_|', '|_|', '|_|', '|_|', '|_|', '|_|', '|_|'],
 		['|_|', '|_|', '|_|', '|_|', '|_|', '|_|', '|_|']]
 
-def makeMove(playerMove):
+def placeMove(playerMove):
 	"""Checks board and places move selection."""
 	for i in xrange(5,-1,-1):
 		if table[i][playerMove[1] - 1] == '|_|':
 			table[i][playerMove[1] - 1] = '|' + playerMove[0] + '|'
-			break
+			return True	
+	return False
+
 
 def player(i):
 	"""Changes player and accepts move selection."""
 	if (i % 2) == 0:
 		print '\nPlayer X.'
-		move = raw_input('Choose a number to drop a piece: ')
-		playerMove = ['X', int(move)]
-		return playerMove
+		return chooseMove('X')
 	else:
 		print '\nPlayer O.'
+		return chooseMove('O')
+
+def chooseMove(player):
+	while True:
 		move = raw_input('Choose a number to drop a piece: ')
-		playerMove = ['O', int(move)]
-		return playerMove
+		try:
+			if int(move) < 1 or int(move) > 7:
+				print '\nChoose a number from 1 to 7.'
+				raw_input('Press ENTER to try again.')
+			else:
+				playerMove = [player, int(move)]
+				return playerMove
+		except ValueError:
+			print '\nInvalid input. Please choose a number from 1 to 7.'
+			raw_input('Press ENTER to try again.')
 
 # Main
 for i in xrange(0, 42):
@@ -39,11 +51,14 @@ for i in xrange(0, 42):
 	for rowNumber in xrange(6):
 		row = ''.join(table[rowNumber])
 		print row
-	makeMove(player(i))
+	while True:
+		if placeMove(player(i)) == False:
+			raw_input('Column is full. Press ENTER and choose another move.')
+		else:
+			break
 
-"""
-if an element is X or O
-	check values around it for same value
-		if value found
+# Need some way to determine if a column is filled and
+# if it is, the player needs to be returned to their move to
+# make a new selection.
 
-"""
+
--- Chapters/5/fourTest.py
+++ Chapters/5/fourTest.py
@@ -9,61 +9,60 @@
 
 
 table = [['|O|', '|_|', '|_|', '|X|', '|X|', '|_|', '|_|'], 
-		['|_|', '|_|', '|X|', '|X|', '|_|', '|_|', '|_|'],
-		['|_|', '|X|', '|X|', '|_|', '|_|', '|O|', '|_|'],
-		['|_|', '|X|', '|_|', '|_|', '|_|', '|_|', '|_|'],
-		['|x|', '|X|', '|_|', '|X|', '|_|', '|_|', '|_|'],
-		['|_|', '|_|', '|_|', '|X|', '|X|', '|X|', '|X|']]
+		['|_|', '|_|', '|X|', '|X|', '|O|', '|_|', '|_|'],
+		['|_|', '|X|', '|X|', '|O|', '|_|', '|O|', '|_|'],
+		['|_|', '|_|', '|O|', '|_|', '|_|', '|_|', '|_|'],
+		['|x|', '|O|', '|_|', '|X|', '|_|', '|_|', '|_|'],
+		['|_|', '|_|', '|_|', '|O|', '|X|', '|X|', '|X|']]
 
 
 
 # Paramters are the starting x and y of the diagonal. 
-def diagCheck(x, y, counter):
-	moves = ''
-	for i in xrange((y + 1)):
-		if i == 0:
-			moves += table[y][i][1]
-		else:
-			y -= 1
-			x += 1
-			moves += table[y][x][1]
-	winner = conditionCheck(moves)
-	if winner == False:
-		"""Holy mother of recursion, Batman."""
-		counter += 1
-		if counter == 1:
-			diagCheck(0, 4, counter)
-		elif counter == 2:
-			diagCheck(0, 5, counter)
-		elif counter == 3: 
-			diagCheck(1, 6, counter)
-	elif counter == 6:
-		return False
+# def diagCheck(x, y, counter):
+# 	moves = ''
+# 	for i in xrange((y + 1)):
+# 		if i == 0:
+# 			moves += table[y][i][1]
+# 		else:
+# 			y -= 1
+# 			x += 1
+# 			moves += table[y][x][1]
+# 	winner = winCheck(moves)
+# 	if winner == False:
+# 		"""Holy mother of recursion, Batman."""
+# 		counter += 1
+# 		if counter == 1:
+# 			diagCheck(0, 4, counter)
+# 		elif counter == 2:
+# 			diagCheck(0, 5, counter)
+# 		elif counter == 3: 
+# 			diagCheck(1, 6, counter)
+# 	elif counter == 6:
+# 		return False
 
-def conditionCheck(moves):
+def winCheck(moves):
 	if 'XXXX' in moves:
 		print 'X'
 	elif 'OOOO' in moves:
-		return 'O'
+		print 'O'
 	else:
 		return False
 
-diagCheck(0, 3, 0)
+# diagCheck(0, 3, 0)
 
 
-# columnMoves = ''
-# for x in xrange(7):
-# 	for y in xrange(6):
-# 		columnMoves += table[y][x][1]
-# print columnMoves
+columnMoves = ''
+for x in xrange(7):
+	for y in xrange(6):
+		columnMoves += table[y][x][1]
+	winCheck(columnMoves)
 
-# rowMoves = ''
-# for y in xrange(6):
-# 	for x in xrange(7):
-# 		rowMoves += table[y][x][1]
-	# if 'XXXX' in rowMoves:
-	# 	print 'Winner!'
-# print rowMoves
+rowMoves = ''
+for y in xrange(6):
+	for x in xrange(7):
+		rowMoves += table[y][x][1]
+	winCheck(rowMoves)
+	
 
 # x = 7
 # y = 6
