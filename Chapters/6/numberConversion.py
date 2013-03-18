2ebbfae5384477e8d83d3641e5a4c7c81ffba2ba
b3m <bminear3@gmail.com>
2013-03-18 16:46:10 -0400

    Conversion function is complete. Needs better interface.

--- Chapters/6/numberConversion.py
+++ Chapters/6/numberConversion.py
@@ -1,17 +1,23 @@
+"""
+Program: Value to Decimal
+Author: Bill Minear
+"""
 
-conversionDictionary = {0:0,1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,
-						10:'A',11:'B',12:'C',13:'D',14:'E',15:'F'}
+conversionDictionary = {'A':10,'B':11,'C':12,'D':12,'E':14,'F':15}
 
-def main(base, value):
-	convertedValue = 0
+def repToDecimal(base, value):
+	convertedTotal = 0
+	# Exponent is -1 for shifting to index 0.
 	exponent = len(value) - 1
 	for digit in value:
-		convertedValue = convertedValue + int(digit) * int(base) ** exponent
+		if str(digit.upper()) in conversionDictionary:
+			digit = conversionDictionary[digit.upper()]
+		convertedValue = int(digit) * int(base) ** exponent
+		convertedTotal = convertedTotal + convertedValue
 		exponent = exponent - 1
-		
-	print "The integer value is", convertedValue 
+	print "The integer value is", convertedTotal
 
 while True:
 	value = raw_input('Enter a number: ')
 	base = raw_input('Enter the base: ')
-	main(base, value)
+	repToDecimal(base, value)
