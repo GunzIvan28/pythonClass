bcd5282c9cbcf8a62ba5e0306313ca149c00952b
b3m <bminear3@gmail.com>
2013-03-31 11:25:39 -0400

    Realized that startingPoint was supposed to be the circle's center point. Modified to adjust for that. Also added some notes about the assignment.

--- Chapters/7/drawCircle.py
+++ Chapters/7/drawCircle.py
@@ -1,3 +1,20 @@
+"""
+Program: drawCircle
+Author: Bill Minear
+
+Notes:
+	+) Noticed some differences between
+		the turtle syntax in the book
+		and what I was able to implement.
+		+) Not sure if there's a version
+			difference or if I've just 
+			missed something.
+		+) Mostly used
+			http://docs.python.org/2/library/turtle.html
+			for syntax and the book for
+			flow control.
+"""
+
 from math import pi
 import turtle
 import os 
@@ -5,33 +22,48 @@ import os
 def clearScreen(): 
     os.system(['clear','cls'][os.name == 'nt']) 
 
-def drawCircle(myTurtle, startingPoint, radius):
+def drawCircle(myTurtle, centerPoint, radius):
+	"""Utilizes arguments to draw a circle."""
 	move = 2.0 * pi * radius / 120.0
+	myTurtle.setheading(90) # Orient turtle North.
 	myTurtle.up()
-	myTurtle.setpos(startingPoint[0], startingPoint[1])
+	myTurtle.setpos((centerPoint[0] + radius), centerPoint[1]) # Move to circle's right outer edge. 
 	myTurtle.down()
 	for count in xrange(120):
-		myTurtle.left(3)
 		myTurtle.forward(move)
+		myTurtle.left(3)
 	myTurtle.mainloop()
 
 def main():
+	"""Program flow control."""
 	clearScreen()
 	while True:
 		try:
 			xAxis = input('X axis starting point: ')
-			yAxis = input('Y axis starting point: ')
-			radius = input('Enter the radius: ')
-			startingPoint = [xAxis, yAxis]
 			break
 		except NameError:
 			print '\nInput must be numerical.'
-			raw_input('Press ENTER to try again.')
+			raw_input('\nPress ENTER to try again.')
+	while True:
+		try:
+			yAxis = input('Y axis starting point: ')
+			break
+		except NameError:
+			print '\nInput must be numerical.'
+			raw_input('\nPress ENTER to try again.')
+	while True:
+		try:
+			radius = input('Enter the radius: ')
+			centerPoint = [xAxis, yAxis]
+			break
+		except NameError:
+			print '\nInput must be numerical.'
+			raw_input('\nPress ENTER to try again.')
 	print "\nWe'll now draw the circle."
-	print 'To exit the program when the circle is complete'
-	print 'just close the window.'
-	raw_input('Press ENTER to continue.')
+	print '\nTo exit the program when the circle'
+	print 'is complete just close the window.'
+	raw_input('\nPress ENTER to continue.')
 	myTurtle = turtle
-	drawCircle(myTurtle, startingPoint, radius)
+	drawCircle(myTurtle, centerPoint, radius)
 
 main()
