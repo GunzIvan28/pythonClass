d21ec4e9e7b4a92d011c6188b112077d2374f442
b3m <bminear3@gmail.com>
2013-04-02 19:49:27 -0400

    Having trouble figuring out how to init a value for the grid and update it without passing it into the button function.

--- Chapters/9/slotMachine.py
+++ Chapters/9/slotMachine.py
@@ -4,13 +4,14 @@ import random
 
 class slotGUI(Frame):
 
+
 	def __init__(self):
 		Frame.__init__(self)
 		self.master.title('Slots!')
 		self.grid()
-		self._initText = Label(self, text = 100)
+		self._initText = Label(self, text = 'Welcome!', height = 5, width = 20, font = 16)
 		self._initText.grid(row = 1, column = 1)
-		self._button = Button(self,text = "Play!", command = self._switch)
+		self._button = Button(self,	text = "Play!", command = self._switch)
 		self._button.grid(row = 2, column = 1)
 
 	def _getRandPic(self):
@@ -19,6 +20,8 @@ class slotGUI(Frame):
 		self._randImage = self._imageDictionary[random.randrange(0,5)]
 		return self._randImage
 
+	# def _updateMoney(self):
+
 	def _switch(self):
 		self._imageList = []
 		self._labelList = []
@@ -26,18 +29,12 @@ class slotGUI(Frame):
 			self._imageList.append(PhotoImage(file = self._getRandPic()))
 			self._labelList.append(Label(self, image = self._imageList[i]))
 			self._labelList[i].grid(row = 1, column = i)
-		self._currentAmount = Label(self, text = '100')
-		self._myMoney = IntVar()
-		if self._myMoney == 0:
-			self._moneyActual = 90
-			self._myMoney.set(self._moneyActual)
-		else:
-			self._moneyActual -= 10
-			self._myMoney.set(self._moneyActual)
-		self._currentAmount = Label(self, textvariable = self._myMoney)
-		self._currentAmount.grid(row = 2, column = 0)
+		self._currentMoney = Label(self, text = money)
+		self._currentMoney.grid(row = 2, column = 0)
+
 
 def main():
+	money = 100
 	beginMessage = 'You begin with $100.\nEach bet is $10.'
 	showinfo(title = 'Welcome!', message = beginMessage)
 	slotGUI().mainloop()
